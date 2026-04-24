d5737d169c8db63fced45aae8c957709f0cf14e9
rmccownISoft <60158596+rmccownISoft@users.noreply.github.com>
2026-04-23 21:06:13 -0500

    feat: reorganized skills files into plugins
    -followed nick nisi's structure for his plugin/skill collection

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  trailingComma: 'all',
+  semi: false,
+  arrowParens: 'avoid',
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+};
